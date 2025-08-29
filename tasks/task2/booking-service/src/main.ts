76c60c0f16da04491c414438ae1e6b736b3d632d
Ekaterina Mulyukina <katchaotic@yandex.com>
2025-08-29 13:12:14 +0300

    task2: инициализация нового сервиса букинга

---
+++ tasks/task2/booking-service/src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
+import { AppModule } from './app.module';
+import path from 'path';
+
+async function bootstrap() {
+    const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+        transport: Transport.GRPC,
+        options: {
+            package: 'booking-service',
+            protoPath: path.resolve(__dirname, './proto/booking.proto'),
+        },
+    });
+
+    await app.listen();
+}
+bootstrap();
