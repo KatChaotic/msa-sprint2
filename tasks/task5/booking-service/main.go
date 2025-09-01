adae4e589192b697cc5abed192321f5914ee64d1
Ekaterina Mulyukina <katchaotic@yandex.com>
2025-09-02 02:30:05 +0300

    task5: реализация canary деплоя

---
+++ tasks/task5/booking-service/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	enableFeatureX := os.Getenv("ENABLE_FEATURE_X") == "true"
+	pongString := os.Getenv("PONG_STRING")
+
+	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, pongString)
+	})
+
+	// TODO: Feature flag route
+	// if ENABLE_FEATURE_X=true, expose /feature
+	if enableFeatureX {
+		http.HandleFunc("/feature", func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "Feature X is enabled!")
+		})
+	}
+
+	log.Println("Server running on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
--- tasks/task5/check-canary.sh
+++ tasks/task5/check-canary.sh
@@ -4,8 +4,20 @@ set -e
 
 echo "▶️ Checking canary release (90% v1, 10% v2)..."
 
-# Посылаем 100 запросов
-for i in {1..100}
+v1_count=0
+v2_count=0
+
+# Посылаем 500 запросов
+for i in {1..500}
 do
-    curl -s http://localhost:9090/ping
+    resp=$(curl -s http://localhost/ping)
+
+    if [[ $resp == *"v1"* ]]; then
+        v1_count=$((v1_count + 1))
+    elif [[ $resp == *"v2"* ]]; then
+        v2_count=$((v2_count + 1))
+    fi
 done
+
+echo "✅ v1: $v1_count ($((v1_count * 100 / 500))%)"
+echo "✅ v2: $v2_count ($((v2_count * 100 / 500))%)"
