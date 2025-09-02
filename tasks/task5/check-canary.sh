d739b8e0fddc6b00b4444b002e1c7cd9ccbf7dbf
Ekaterina Mulyukina <katchaotic@yandex.com>
2025-09-02 03:23:46 +0300

    task5: расширение тестов для корректной проверки

--- tasks/task5/check-canary.sh
+++ tasks/task5/check-canary.sh
@@ -17,7 +17,25 @@ do
     elif [[ $resp == *"v2"* ]]; then
         v2_count=$((v2_count + 1))
     fi
+
+    if ((i % 100 == 0)); then
+        echo "▶️ $(($i * 100 / 500)) % complete"
+    fi
 done
 
-echo "✅ v1: $v1_count ($((v1_count * 100 / 500))%)"
-echo "✅ v2: $v2_count ($((v2_count * 100 / 500))%)"
+p1=$((v1_count * 100 / 500))
+p2=$((v2_count * 100 / 500))
+
+# Погрешность +/- 5%
+
+if [[ $p1 -gt 85 ]] && [[ $p1 -lt 95 ]]; then
+    echo "✅ v1: $v1_count ($p1%)"
+else
+    echo "❌ v1: $v1_count ($p1%)"
+fi
+
+if [[ $p2 -gt 5 ]] && [[ $p2 -lt 15 ]]; then
+    echo "✅ v2: $v2_count ($p2%)"
+else
+    echo "❌ v2: $v2_count ($p2%)"
+fi
